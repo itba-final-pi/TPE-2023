875ba63dc7d0b6740c26e647f370088c1fca09aa
Tomás Pietravallo <hello@tomaspietravallo.com>
2023-12-01 13:56:28 -0300

    test: add empty tests

---
+++ tests/files/template.test.c
@@ -0,0 +1,17 @@
+// This file is meant to be a template for all other tests
+// This file is excluded from test runs and is not counted towards the total amount of tests ran
+
+#undef NDEBUG
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "tests.h"
+
+int
+main(void) {
+    assert(1 == 1);
+
+    return 0;
+}
---
+++ tests/tests.h
@@ -0,0 +1,5 @@
+// Do not remove this check.
+// Meant to prevent tests from getting compiled with production code accidentally 👇
+#ifndef DEBUG_RUN_TESTS
+    #error "File was not compiled as test. Be careful NOT to treat these the same as production. Do not compile tests manually, use the ./test.sh file"
+#endif
