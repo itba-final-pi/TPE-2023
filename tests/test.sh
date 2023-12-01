d20bc268a46bc6469da5ad0406f35b069bdb0a1a
Tomás Pietravallo <hello@tomaspietravallo.com>
2023-12-01 14:21:56 -0300

    fix: ci file not found

--- tests/test.sh
+++ tests/test.sh
@@ -7,7 +7,7 @@ do
     echo "$file" | egrep '.*template.*' -q
     if [[ "$?" -ne 0 && $ERROR -eq 0 ]]
     then
-        gcc "$file" -DDEBUG_RUN_TESTS -Wall -pedantic -std=c99 -Werror -fsanitize=address -o test.out
+        gcc "./files/$file" -DDEBUG_RUN_TESTS -Wall -pedantic -std=c99 -Werror -fsanitize=address -o test.out
         GCC_EXITC=$?
         if [ $GCC_EXITC -ne 0 ]
         then
