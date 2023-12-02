ba95a706bffe4508bcaed1f193a2069fb1f559be
Tomás Pietravallo <hello@tomaspietravallo.com>
2023-12-02 19:54:44 -0300

    feat: add required functions to BikeStation

--- headers/BikeStation.h
+++ headers/BikeStation.h
@@ -14,4 +14,28 @@ typedef struct BikeStationCDT {
 
 typedef BikeStationCDT * BikeStation;
 
+// Creates a new BikeStation
+BikeStation newBikeStation(size_t id, char * name);
+
+void setLatitude(BikeStation station, double latitude);
+
+void setLongitude(BikeStation station, double longitude);
+
+void incrementMemberTrips(BikeStation station);
+
+void incrementCasualTrips(BikeStation station);
+
+size_t getAllTips(BikeStation station);
+
+size_t getMemberTrips(BikeStation station);
+
+size_t getCasualTrips(BikeStation station);
+
+// Returns a **COPY** of the station name
+char * getName(BikeStation station);
+
+double getLatitude(BikeStation station);
+
+double getLongitude(BikeStation station);
+
 #endif
--- src/BikeStation.c
+++ src/BikeStation.c
@@ -1,2 +1,39 @@
 // @todo #2 Refactor import paths
+#include <string.h>
+
 #include "../headers/BikeStation.h"
+
+BikeStation newBikeStation(size_t id, char * name) {
+    BikeStation new = calloc(1, sizeof(BikeStationCDT));
+    new->id = id;
+    strcpy(new->name, name);
+    return new;
+}
+
+void setLatitude(BikeStation station, double latitude) {
+    station->latitude = latitude;
+}
+
+void setLongitude(BikeStation station, double longitude){
+    station->longitude = longitude;
+}
+
+void incrementMemberTrips(BikeStation station) {
+    station->memberTrips++;
+};
+
+void incrementCasualTrips(BikeStation station) {
+    station->casualTrips++;
+};
+
+size_t getAllTips(BikeStation station) {
+    return station->memberTrips + station->casualTrips;
+}
+
+size_t getMemberTrips(BikeStation station) {
+    return station->memberTrips;
+}
+
+size_t getCasualTrips(BikeStation station) {
+    return station->casualTrips;
+}
