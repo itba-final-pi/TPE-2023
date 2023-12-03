b341a8c6ef3a31d99329e882ef089f7f5e5ae0ee
Santiago Torres <torres.kilmes@gmail.com>
2023-12-03 19:35:13 -0300

    test: create a basic test for CityStations

--- tests/files/citystations.test.c
+++ tests/files/citystations.test.c
@@ -12,8 +12,18 @@
 
 int
 main(void) {
+    
+    BikeStation station = newBikeStation(1286, "Evans / Clark");
+    setLatitude(station, 45.5110837);
+    setLongitude(station, -73.5679775);
+
     CityStations new = newCityStations("./tests/data/test.csv", "");
     assert( getStationsCount(new) == 9 );
+    assert( getStation(new, 1286) != NULL );
+    assert( compareStationsByName(getStation(new, 1286), station) == 0 );
+
+    freeStation(station);
+
     freeCityStations(new);
     return 0;
 }
