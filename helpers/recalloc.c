30333dc97a5d3764ad6a2a1e7caa424a80a223d6
luca mancuso <lmancuso@itba.edu.ar>
2023-12-02 22:37:07 -0300

    docs: translate comment to english

--- helpers/recalloc.c
+++ helpers/recalloc.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-// Hace un realloc de vec pero con todos los nuevos elementos inicializados en 0
+// Reallocs vec but setting all new fields to 0.
 void * recalloc(void * vec, size_t len, size_t new_len){
 	void * new = realloc(vec, new_len);
 
