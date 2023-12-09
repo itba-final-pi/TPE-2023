e153369679aafc73a1b49b091a25770da5685644
Santiago Torres <torres.kilmes@gmail.com>
2023-12-08 23:38:19 -0300

    test: update citystations test to work with MON and NYC at the same time

--- tests/files/citystations.test.c
+++ tests/files/citystations.test.c
@@ -88,5 +88,77 @@ main(void) {
     freeFileHandler(file);
 #endif
 
+#ifdef NYC
+    CityStations new = newCityStations();
+    
+    FileHandler file = newFileHandler("./Datasets Alumnos/stationsNYC.csv");
+
+    char * line = getNextLine(file); // ignore header line
+
+    // While there's a line, AND it is not empty
+    while( hasNextLine(file) && (line = getNextLine(file)) ) {
+        loadStation(new, line);
+    }
+
+    BikeStation station = newBikeStation(728804, "Riverside Dr & W 72 St", 40.7805779901033, -73.9856243133545);
+
+    assert( getStationsCount(new) == 1680 );
+    assert( getStation(new, 728804) != NULL );
+    assert( compareStationsByName(getStation(new, 728804), station) == 0 );
+    assert( getLongitude(getStation(new, 728804)) == getLongitude(station) );
+    assert( getLatitude(getStation(new, 728804)) == getLatitude(station) );
+
+    freeFileHandler(file);
+    //process trip
+    file = newFileHandler("./Datasets Alumnos/bikesNYC.csv");
+    line = getNextLine(file); // ignore header line
+
+    // While there's a line, AND it is not empty
+    while( hasNextLine(file) && (line = getNextLine(file)) ) {
+        processTrip(new, line);
+    }
+
+    orderStationsByTrips(new);
+    size_t total_stations = getStationsCount(new);
+
+    size_t i = 0;
+    char * aux = calloc(1, sizeof(char));
+
+    toBeginAlphabeticOrder(new);
+
+    while(hasNextAlphabeticOrder(new)) {
+        BikeStation station = nextAlphabeticOrder(new);
+        assert(station != NULL);
+
+        char * name = getName(station);
+        assert(name != NULL);
+
+        assert( strcasecmp(aux, name) <= 0 );
+        free(aux);
+        aux = name;
+        i++;
+    }
+
+    free(aux);
+
+    assert(i == total_stations);
+
+    size_t total_trips_aux = -1;
+    i = 0;
+    toBeginTripsOrder(new);
+    while(hasNextTripsOrder(new)) {
+        BikeStation station = nextTripsOrder(new);
+        assert(station != NULL);
+        assert(total_trips_aux >= getAllTrips(station));
+        total_trips_aux = getAllTrips(station);
+        i++;
+    }
+    assert(i == total_stations);
+
+    freeStation(station);
+    freeCityStations(new);
+    freeFileHandler(file);
+#endif
+
     return 0;
 }
