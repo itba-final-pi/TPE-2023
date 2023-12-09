58966f867c2af5491aee1197f2217b87efcf647f
Tomás Pietravallo <hello@tomaspietravallo.com>
2023-12-09 15:02:13 -0300

    docs: add descriptions and references

--- headers/BinaryTree.h
+++ headers/BinaryTree.h
@@ -4,10 +4,20 @@
 #define _binary_tree_h
 
 typedef struct NodeAVL * AVLTree;
+
+/**
+ * Balanced Binary Search Tree implementation.
+ * 
+ * Balancing is achieved by using an AVL Tree, based on the book "Programming Abstractions in C" by Eric S. Roberts
+ * 
+ * Special thanks to the University of San Francisco, California, for [their AVLTree Visualization tool](https://www.cs.usfca.edu/~galles/visualization/AVLtree.html)
+ */
 typedef struct BinaryTreeCDT * BinaryTree;
 
 /**
  * @brief Creates a new BinaryTree ADT
+ * 
+ * Allows storing key-value pairs, where the key is `size_t` and the value is a void pointer
  * 
  * @return BinaryTree 
  */
@@ -32,10 +42,31 @@ void insert(BinaryTree * t, size_t key, void * elem);
  */
 void * search(BinaryTree t, size_t key);
 
+/**
+ * @brief Prepare the binary tree to start in-order interation
+ * 
+ * @param t BinaryTree
+ */
 void toBeginTreeIter(BinaryTree t);
 
+/**
+ * @brief Returns whether or not another element exists in the in-order tree traversal
+ * 
+ * @param t BinaryTree
+ * @return int (1 if an element exists, 0 if the traversal was finished)
+ */
 int hasNextTreeElem(BinaryTree t);
 
+/**
+ * @brief Get the next element on the in-order tree traversal
+ * 
+ * Returns the **POINTER** that was stored
+ * 
+ * NULL if no element exists (or NULL was stored in the first place)
+ * 
+ * @param t BinaryTree
+ * @return void * 
+ */
 void * getNextTreeElem(BinaryTree t);
 
 #ifdef DEBUG_RUN_TESTS
@@ -48,7 +79,7 @@ AVLTree getRightNode(AVLTree t);
 
 size_t getNodeKey(AVLTree t);
 
-void * getNodeElem(AVLTree t);
+void * getNodeElement(AVLTree t);
 
 #endif
 
